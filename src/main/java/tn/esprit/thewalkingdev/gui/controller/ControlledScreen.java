5c9fe00ad6237febc1c62e6313abce711d9c0443
hugoboj <khalil.boujmil@esprit.tn>
2015-10-13 19:34:34 +0200

    classe lel navigabilité

---
+++ src/main/java/tn/esprit/thewalkingdev/gui/controller/ControlledScreen.java
@@ -0,0 +1,10 @@
+package tn.esprit.thewalkingdev.gui.controller;
+
+public interface ControlledScreen {
+
+    /**
+     *
+     * @param screenPage
+     */
+    public void setScreenParent(ScreensController screenPage);
+}    
